52a86db7380d8d4b0c1ef2f836e5b3b3202a7363
Nguyen Phan <nqphan212@gmail.com>
2015-09-15 13:40:07 -0400

    connect obstacles together

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DoorTrigger : MonoBehaviour {
 
 	public Door door;
+	public bool ignoreTrigger;
 	// Use this for initialization
 	void Start () {
 	
@@ -15,15 +16,30 @@ public class DoorTrigger : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D target){
+
+		if (ignoreTrigger)
+			return;
+
 		if(target.gameObject.tag == "Player"){
 			door.Open();
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D target){
+
+		if (ignoreTrigger)
+			return;
+
 		if(target.gameObject.tag == "Player"){
 			door.Close();
 		}
+	}
+
+	public void Toggle(bool value){
+		if(value)
+			door.Open();
+		else
+			door.Close();
 	}
 
 }
--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Switch : MonoBehaviour {
 
+	public DoorTrigger[] doorTriggers;
+	public bool sticky;
+
+	private bool down;
 	private Animator animator;
 
 	// Use this for initialization
@@ -18,9 +22,25 @@ public class Switch : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D target){
 		animator.SetInteger ("AnimState",1);
+		down = true;
+
+		foreach(DoorTrigger trigger in doorTriggers){
+			if(trigger != null)
+				trigger.Toggle(true);
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D target){
+		if (sticky && down)
+			return;
+
 		animator.SetInteger ("AnimState", 2);
+
+		down = false;
+
+		foreach(DoorTrigger trigger in doorTriggers){
+			if(trigger != null)
+				trigger.Toggle(false);
+		}
 	}
 }
