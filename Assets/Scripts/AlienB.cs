13c40d108f30aea89ffbd066c5af986b6a43761d
Nguyen Phan <nqphan212@gmail.com>
2015-09-16 17:55:32 -0400

    Creat alien C animation. Using StartCoroutine to set up a fixed time for shooting

--- Assets/Scripts/AlienB.cs
+++ Assets/Scripts/AlienB.cs
@@ -2,20 +2,39 @@
 using System.Collections;
 
 public class AlienB : MonoBehaviour {
-
+	
 	private Animator animator;
-
+	
+	private bool readyToAttack;
+	
 	// Use this for initialization
 	void Start () {
-		animator = GetComponent<Animator>();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
-	void OnTriggerEnter2D(Collider2D target){
-		animator.SetInteger ("AnimState", 1);
+	
+	void OnTriggerStay2D(Collider2D target){
+		if (target.gameObject.tag == "Player") {
+			if(readyToAttack){
+				var explode = target.GetComponent<Explode>() as Explode;
+				explode.OnExplode();
+				
+			}else{
+				animator.SetInteger ("AnimState", 1);
+			}
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D target){
+		readyToAttack = false;
+		animator.SetInteger ("AnimState", 0);
+	}
+	
+	void Attack(){
+		readyToAttack = true;
 	}
 }
---
+++ Assets/Scripts/AlienC.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienC : MonoBehaviour {
+	
+	public float attackDelay = 3f;
+	
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		
+		animator = GetComponent<Animator> ();
+		
+		if (attackDelay > 0) {
+			StartCoroutine(OnAttack());
+			print("I am here 1\t");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetInteger ("AnimState", 0);
+	}
+	
+	IEnumerator OnAttack(){
+		yield return new WaitForSeconds(attackDelay);
+		print("I am here 2");
+		Fire ();
+		StartCoroutine (OnAttack ());
+	}
+	
+	void Fire(){
+		print("I am here 3");
+		animator.SetInteger ("AnimState", 1);
+	}
+}
